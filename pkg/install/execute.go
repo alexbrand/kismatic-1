c884003ae7d3ba53a746ce105a0702c03adb5700
Alexander Brand <abrand@apprenda.com>
2017-08-10 15:49:38 -0400

    Refactored upgrade code

--- pkg/install/execute.go
+++ pkg/install/execute.go
@@ -486,7 +486,7 @@ func (ae *ansibleExecutor) UpgradeNodes(plan Plan, nodesToUpgrade []ListableNode
 	// Upgrade etcd nodes
 	for _, nodeToUpgrade := range nodesToUpgrade {
 		for _, role := range nodeToUpgrade.Roles {
-			if role == "tcd" {
+			if role == "cd" {
 				node := nodeToUpgrade
 				if err := ae.upgradeNodes(plan, onlineUpgrade, node); err != nil {
 					return fmt.Errorf("error upgrading node %q: %v", node.Node.Host, err)
